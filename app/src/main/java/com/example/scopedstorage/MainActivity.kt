602bcef859491a83ca9fcbee0b09fea00d1d9440
Kimsohyun93 <shk980903@gmail.com>
2021-01-04 18:46:35 +0900

    feature(MainAcitivity, Gradle, Manifest): Camera Permission
    add camera permission (tedpermission)

--- app/src/main/java/com/example/scopedstorage/MainActivity.kt
+++ app/src/main/java/com/example/scopedstorage/MainActivity.kt
@@ -1,26 +1,22 @@
 package com.example.scopedstorage
 
-import android.app.Activity
+import android.Manifest
 import android.content.Intent
-import android.graphics.Bitmap
-import android.net.Uri
 import android.os.Bundle
-import android.provider.MediaStore
-import android.util.Log
-import android.view.View
-import android.widget.Button
-import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.example.scopedstorage.databinding.ActivityMainBinding
+import com.gun0912.tedpermission.PermissionListener
+import com.gun0912.tedpermission.TedPermission
+
 
 //import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     private val OPEN_GALLERY = 1
-    private lateinit var binding: ActivityMainBinding
+    lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,8 +25,27 @@ class MainActivity : AppCompatActivity() {
         setContentView(view)
 //        val go_intent = findViewById(R.id.loadImageBtn) as Button
         binding.loadImageBtn.setOnClickListener{ openGallery() }
+        TedPermission.with(this)
+            .setPermissionListener(permissionlistener)
+            .setDeniedMessage("If you reject permission,you can not use this service\n\nPlease turn on permissions at [Setting] > [Permission]")
+            .setPermissions(Manifest.permission.CAMERA)
+            .check();
+
     }
 
+    var permissionlistener: PermissionListener = object : PermissionListener {
+        override fun onPermissionGranted() {
+            Toast.makeText(this@MainActivity, "Permission Granted", Toast.LENGTH_SHORT).show()
+        }
+
+        override fun onPermissionDenied(deniedPermissions: List<String>) {
+            Toast.makeText(
+                this@MainActivity,
+                "Permission Denied\n$deniedPermissions",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+    }
     private fun openGallery(){
         val intent: Intent = Intent(Intent.ACTION_GET_CONTENT)
         intent.setType("image/*")
@@ -43,14 +58,14 @@ class MainActivity : AppCompatActivity() {
 
 //        val imageView = findViewById(R.id.imageView) as ImageView
         if(requestCode == OPEN_GALLERY){
+            var dataUri = data?.data
             if(resultCode == RESULT_OK){
-                var dataUri = data?.data
                 try{
                    Glide.with(this).load(dataUri).into(binding.imageView)
                 }catch (e:Exception){
                     Toast.makeText(this,"$e",Toast.LENGTH_SHORT).show()
                 }
-            }else {
+            }else{
 
             }
         }
