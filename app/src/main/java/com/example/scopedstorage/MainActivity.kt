4b926066ab33c41e930be8e448b847101333da86
Kimsohyun93 <shk980903@gmail.com>
2021-01-05 14:31:04 +0900

    feature(MainActivity, AndroidManifest, file_paths): implement Camera capture
    implement Camera capture use FileProvider
    
    - unresolved issue :
    picture gets rotated
    picture save function

--- app/src/main/java/com/example/scopedstorage/MainActivity.kt
+++ app/src/main/java/com/example/scopedstorage/MainActivity.kt
@@ -2,19 +2,30 @@ package com.example.scopedstorage
 
 import android.Manifest
 import android.content.Intent
+import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
+import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.FileProvider
 import com.bumptech.glide.Glide
 import com.example.scopedstorage.databinding.ActivityMainBinding
 import com.gun0912.tedpermission.PermissionListener
 import com.gun0912.tedpermission.TedPermission
+import java.io.File
+import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.*
 
 
 //import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
+    private val OPEN_CAMERA = 0
     private val OPEN_GALLERY = 1
     lateinit var binding: ActivityMainBinding
 
@@ -24,7 +35,8 @@ class MainActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 //        val go_intent = findViewById(R.id.loadImageBtn) as Button
-        binding.loadImageBtn.setOnClickListener{ openGallery() }
+        binding.loadImageBtn.setOnClickListener { openGallery() }
+        binding.takePhotoBtn.setOnClickListener { openCamera() }
         TedPermission.with(this)
             .setPermissionListener(permissionlistener)
             .setDeniedMessage("If you reject permission,you can not use this service\n\nPlease turn on permissions at [Setting] > [Permission]")
@@ -46,31 +58,102 @@ class MainActivity : AppCompatActivity() {
             ).show()
         }
     }
-    private fun openGallery(){
+
+    private fun openGallery() {
         val intent: Intent = Intent(Intent.ACTION_GET_CONTENT)
         intent.setType("image/*")
-        startActivityForResult(intent,OPEN_GALLERY)
+        startActivityForResult(intent, OPEN_GALLERY)
+    }
+
+    private fun openCamera() {
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+            takePictureIntent.resolveActivity(packageManager)?.also {
+                val photoFile: File? = try {
+                    createImageFile()
+                } catch (ex: IOException) {
+                    Log.e("Error : ", "createImageFile error")
+                    null
+                }
+                photoFile?.also {
+                    val photoUri: Uri = FileProvider.getUriForFile(
+                        this,
+                        "com.example.android.fileprovider",
+                        it
+                    )
+                    takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri)
+                    startActivityForResult(takePictureIntent, OPEN_CAMERA)
+                }
+                //   startActivityForResult(takePictureIntent, OPEN_CAMERA)
+            }
+        }
+        /*
+    if(intent.resolveActivity(getPackageManager()) != null) {
+        startActivityForResult(intent, OPEN_CAMERA)
+    }
+
+     */
+    }
+
+    lateinit var currentPhotoPath: String
+
+    @Throws(IOException::class)
+    private fun createImageFile(): File? {
+        val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        val storageDir: File? = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        return File.createTempFile(
+            "JPEG_${timeStamp}_", /* prefix */
+            ".jpg", /* suffix */
+            storageDir /* directory */
+        ).apply {
+            currentPhotoPath = absolutePath
+        }
+
     }
 
     @Override
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-
 //        val imageView = findViewById(R.id.imageView) as ImageView
-        if(requestCode == OPEN_GALLERY){
-            var dataUri = data?.data
-            if(resultCode == RESULT_OK){
-                try{
-                   Glide.with(this).load(dataUri).into(binding.imageView)
-                }catch (e:Exception){
-                    Toast.makeText(this,"$e",Toast.LENGTH_SHORT).show()
-                }
-            }else{
 
+        if (requestCode == OPEN_GALLERY) {
+            if (resultCode == RESULT_OK) {
+                try {
+                    var dataUri = data?.data
+                    Glide.with(this).load(dataUri).into(binding.imageView)
+                    Log.d("## Data Uri:  ", "$dataUri")
+                } catch (e: Exception) {
+                    Toast.makeText(this, "$e", Toast.LENGTH_SHORT).show()
+                }
+            }
+        } else if (requestCode == OPEN_CAMERA) {
+            if (resultCode == RESULT_OK) {
+                try {
+                    val bmOptions = BitmapFactory.Options()
+                    BitmapFactory.decodeFile(currentPhotoPath, bmOptions)?.also { bitmap ->
+                        binding.imageView.setImageBitmap(bitmap)}
+
+                        //load thumbnail
+                        //val imageBitmap = data?.extras?.get("data") as Bitmap
+                        //binding.imageView.setImageBitmap(imageBitmap)
+                        /* fail load image to glide
+                        var dataUri: Uri? = data?.data
+                        Log.e("##### Data Uri:  ", "$dataUri")
+                        Glide.with(this).load(dataUri).into(binding.imageView)
+
+                        //binding.imageView.setImageURI(dataUri)
+                         */
+
+                    } catch (e: Exception) {
+                        Toast.makeText(this, "$e", Toast.LENGTH_SHORT).show()
+                        Log.e("Error", "${e.message}")
+                    }
+                }
+            } else {
             }
         }
+
     }
-}
+
 
 //
 //
